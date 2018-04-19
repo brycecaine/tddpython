748a015ce7a748a1beb892cfa12e13ea2be4a18f
Bryce Caine <brycecaine@gmail.com>
2018-04-19 04:57:21 +0000

    Developed test with comments and unittest

---
+++ functional_tests_orig.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.add_argument("--headless")
+browser = webdriver.Firefox(firefox_options=options)
+browser.get('http://35.185.203.248:8000/')
+# browser.get('http://35.185.203.248:8000')
+# browser.get('http://10.138.0.3:8000')
+
+assert 'Django' in browser.title
