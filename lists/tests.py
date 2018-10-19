eba97bfc5050f696ee1def33cfb70f39379924b6
Bryce Caine <brycecaine@gmail.com>
2018-10-19 22:26:53 +0000

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
@@ -13,7 +14,5 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
